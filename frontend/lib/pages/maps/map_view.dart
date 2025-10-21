291a341a56f3556c0888d03022665fa1eb21cf43
AidonPatrao <aidonpatrao073@gmail.com>
2025-10-22 01:40:23 +0530

    initial maps commit

--- frontend/lib/pages/maps/map_view.dart
+++ frontend/lib/pages/maps/map_view.dart
@@ -3,9 +3,14 @@
 
 import 'package:flutter/material.dart';
 
-class MapView extends StatelessWidget {
+class MapView extends StatefulWidget {
   const MapView({super.key});
 
+  @override
+  State<MapView> createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
